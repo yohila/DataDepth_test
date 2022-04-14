5d4da95d297e3d6e66966b02312da65ad68af1de
yohila <awais_hec@yahoo.com>
2022-04-14 12:58:48 +0200

    Upload all OS

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ sys.platform
 import platform
 
 
-if sys.platform=='linux':
-	setup(
+# if sys.platform=='linux':
+setup(
     	name="depth",
    	version="1.0.0",
     	author="Pavlo mozharovskyi",
@@ -25,45 +25,45 @@ if sys.platform=='linux':
    	zip_safe=False,
 	)
 
-if sys.platform=='nt' and platform.architecture()[0] == "32bit":
-	setup(
-    	name="depth",
-    	version="1.0.0",
-    	author="Pavlo mozharovskyi",
-    	author_email="pavlo.mozharovskyi@telecom-paris.fr",
-   	description="The package provides many procedures for calculating the depth of points in an empirical distribution for many notions of data depth",
-   	# long_description=long_description,
-   	# long_description_content_type="text/markdown",
-   	url="https://github.com/pypa",
-    	packages=find_packages(),
-    	ext_modules=[
+# if sys.platform=='nt' and platform.architecture()[0] == "32bit":
+# 	setup(
+#     	name="depth",
+#     	version="1.0.0",
+#     	author="Pavlo mozharovskyi",
+#     	author_email="pavlo.mozharovskyi@telecom-paris.fr",
+#    	description="The package provides many procedures for calculating the depth of points in an empirical distribution for many notions of data depth",
+#    	# long_description=long_description,
+#    	# long_description_content_type="text/markdown",
+#    	url="https://github.com/pypa",
+#     	packages=find_packages(),
+#     	ext_modules=[
         
-   	 ],
-    	include_package_data=True,
-  	  package_data={"win32": 		['ddalpha.dll','depth_wrapper.so','libatomic-1.dll','libgcc_s_seh-1.dll','libgfortran-3.dll','libgmp-10.dll','libgmpxx-4.dll','libgomp-1.dll','libgomp-plugin-host_nonshm-1.dll','libquadmath-0.dll','libssp-0.dll','libstdc++-6.dll','libvtv-0.dll','libvtv_stubs-0.dll','libwinpthread-1.dll']},
-   	 zip_safe=False,
-	)
+#    	 ],
+#     	include_package_data=True,
+#   	  package_data={"win32": 		['ddalpha.dll','depth_wrapper.so','libatomic-1.dll','libgcc_s_seh-1.dll','libgfortran-3.dll','libgmp-10.dll','libgmpxx-4.dll','libgomp-1.dll','libgomp-plugin-host_nonshm-1.dll','libquadmath-0.dll','libssp-0.dll','libstdc++-6.dll','libvtv-0.dll','libvtv_stubs-0.dll','libwinpthread-1.dll']},
+#    	 zip_safe=False,
+# 	)
 
 
 
-if sys.platform=='nt' and platform.architecture()[0] == "64bit":
-	setup(
-    	name="depth",
-    	version="1.0.0",
-    	author="Pavlo mozharovskyi",
-    	author_email="pavlo.mozharovskyi@telecom-paris.fr",
-   	description="The package provides many procedures for calculating the depth of points in an empirical distribution for many notions of data depth",
-   	# long_description=long_description,
-   	# long_description_content_type="text/markdown",
-   	url="https://github.com/pypa",
-    	packages=find_packages(),
-    	ext_modules=[
+# if sys.platform=='nt' and platform.architecture()[0] == "64bit":
+# 	setup(
+#     	name="depth",
+#     	version="1.0.0",
+#     	author="Pavlo mozharovskyi",
+#     	author_email="pavlo.mozharovskyi@telecom-paris.fr",
+#    	description="The package provides many procedures for calculating the depth of points in an empirical distribution for many notions of data depth",
+#    	# long_description=long_description,
+#    	# long_description_content_type="text/markdown",
+#    	url="https://github.com/pypa",
+#     	packages=find_packages(),
+#     	ext_modules=[
         
-   	 ],
-    	include_package_data=True,
-  	  package_data={"win64": 		['ddalpha.dll','depth_wrapper.so','libatomic-1.dll','libgcc_s_seh-1.dll','libgfortran-3.dll','libgmp-10.dll','libgmpxx-4.dll','libgomp-1.dll','libgomp-plugin-host_nonshm-1.dll','libquadmath-0.dll','libssp-0.dll','libstdc++-6.dll','libvtv-0.dll','libvtv_stubs-0.dll','libwinpthread-1.dll']},
-   	 zip_safe=False,
-	)
+#    	 ],
+#     	include_package_data=True,
+#   	  package_data={"win64": 		['ddalpha.dll','depth_wrapper.so','libatomic-1.dll','libgcc_s_seh-1.dll','libgfortran-3.dll','libgmp-10.dll','libgmpxx-4.dll','libgomp-1.dll','libgomp-plugin-host_nonshm-1.dll','libquadmath-0.dll','libssp-0.dll','libstdc++-6.dll','libvtv-0.dll','libvtv_stubs-0.dll','libwinpthread-1.dll']},
+#    	 zip_safe=False,
+# 	)
 
 
 
